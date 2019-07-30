f1ebdeb37b46021fc5d5147e45eae9e132ca70a2
ZhenyuGeng <freergzy8212@hotmail.com>
2019-07-30 23:22:34 +0200

    update

--- main/views/__init__.py
+++ main/views/__init__.py
@@ -2,7 +2,9 @@ from flask import Blueprint
 from flask_restplus import Api
 
 from .user import api as user_api
+from .shop import api as shop_api
 
 blueprint = Blueprint('api', __name__)
 api = Api(blueprint)
 api.add_namespace(user_api)
+api.add_namespace(shop_api)
